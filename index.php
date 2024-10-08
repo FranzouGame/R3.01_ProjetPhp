de9a7c14f41349de2d0097a34209ad9728f52850
François Barlic <franz04@outlook.fr>
2024-10-08 10:30:11 +0200

    Modification thème et couleur navbar

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Paradi de l'aspi</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
