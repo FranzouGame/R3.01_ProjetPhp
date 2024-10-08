52f09b6d8b73c2aeac0d8dd6d32ac36c45b3379e
François Barlic <franz04@outlook.fr>
2024-10-08 14:52:53 +0200

    Modification de la génération des cartes avec la bd

--- connexion.php
+++ connexion.php
@@ -6,22 +6,23 @@ $connexionData = json_decode($connexion, true);
 
 
 // Test des connexions possibles
-    /* Nécéssaire car nous travaillons sur localhost et sur 
+/* Nécéssaire car nous travaillons sur localhost et sur 
     lakartxela en fonction de la base de donnée disponible */
-foreach ($connexionData["hosts"] as $hosts)
-{
+foreach ($connexionData["hosts"] as $hosts) {
     // Récupération des données de connexion
-    $host = $hosts;
-    $bdd = $hosts[0];
-    $usr = $hosts[1];
-    $pwd = $hosts[2];
+    $host = $hosts[0];
+    $bdd = $hosts[1];
+    $usr = $hosts[2];
+    $pwd = $hosts[3];
 
-    // Test de la connexion : 
-    $link = mysqli_connect($host,$user,$pass,$bdd);
-    if ($link != "")
-    {   
-        break;
+    // Connexion à la base de données
+    $link = mysqli_connect($host, $usr, $pwd, $bdd);
+
+    if ($link) {
+        break; // On sort de la boucle si la connexion est réussie
     }
-}    
+}
 
-?>
+if (!$link) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
--- index.php
+++ index.php
@@ -32,32 +32,65 @@
     </header>
 
     <h1 class="container mt-5">Articles : </h1>
+    <?php
+    // Inclusion du fichier de connexion
+    include 'connexion.php';
 
-    <main class="container">
-        <div class="row justify-content-center">
-            <!-- Première carte -->
-            <div class="col-md-3 mb-4 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="images/images.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+    // Requête SQL pour récupérer les produits
+    $sql = "SELECT idProd, libelle, descriptif, image, vignette FROM produit";
+    $result = mysqli_query($link, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo '<main class="container"><div class="row justify-content-center">';
+
+        // Boucle pour chaque produit
+        while ($row = mysqli_fetch_assoc($result)) {
+            // Génération de la carte pour chaque produit
+            echo '
+        <div class="col-md-3 mb-4 d-flex justify-content-center">
+            <div class="card clickable-card" data-bs-toggle="modal" data-bs-target="#modal' . $row["idProd"] . '" style="width: 18rem;">
+                <img src="' . $row["vignette"] . '" class="card-img-top" alt="' . $row["libelle"] . '">
+                <div class="card-body">
+                    <h5 class="card-title">' . $row["libelle"] . '</h5>
+                    <p class="card-text">' . substr($row["descriptif"], 0, 100) . '...</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modale pour le produit ' . $row["idProd"] . ' -->
+        <div class="modal fade" id="modal' . $row["idProd"] . '" tabindex="-1" aria-labelledby="modalLabel' . $row["idProd"] . '" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel' . $row["idProd"] . '">' . $row["libelle"] . '</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <img src="' . $row["image"] . '" class="img-fluid mb-3" alt="' . $row["libelle"] . '">
+                        <p>' . $row["descriptif"] . '</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
                     </div>
                 </div>
             </div>
-            <!-- Deuxieme carte -->
-            <div class="col-md-3 mb-4 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="images/images.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-    </main>
+        </div>';
+        }
+
+        echo '</div></main>';
+    } else {
+        echo "Aucun produit trouvé.";
+    }
+
+    // Fermeture de la connexion
+    mysqli_close($link);
+    ?>
+
+
+    <!-- Bootstrap 4.6.2 JS and dependencies (jQuery and Popper.js) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.min.js"></script>
 
 </body>
 
