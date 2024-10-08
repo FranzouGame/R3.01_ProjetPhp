8ac44fdc85c67322c34d8db842e48c7bb22b6de6
François Barlic <franz04@outlook.fr>
2024-10-08 16:51:17 +0200

    Retrait git ignore, rajout d'un aspirateur

--- bd.sql
+++ bd.sql
@@ -19,4 +19,6 @@ identifiant varchar(16) PRIMARY KEY,
 mdp varchar(16));
 
 INSERT INTO produit (idProd, libelle, descriptif, image, vignette) VALUES
+(1, 'Henry', 'Découvrez Henry, laspirateur qui redéfinit le nettoyage. Conçu pour offrir une performance inégalée, Henry est le choix idéal pour les ménages et les professionnels en quête defficacité.', 'Images/images.jpg', '')
 (2, 'Noo Noo', 'Noo-Noo est un aspirateur de couleur bleu vif avec un long tuyau flexible qui se termine par un embout en forme de trompe. Son corps est arrondi et il possède des yeux expressifs qui lui donnent une personnalité amicale et curieuse.', 'Images/Noonoo_1-300x224.jpg', '')
+(3, 'MegaAspi3000x', 'Le MegaAspi3000x est le choix idéal pour ceux qui recherchent un aspirateur puissant, efficace et facile à utiliser. Que ce soit pour un nettoyage quotidien ou des tâches plus lourdes, il répondra à toutes vos attentes en matière de propreté.', 'Images/megaaspi3000x.jpg', '')
