daf17940198528f685b9e83ef4d3c458d470c904
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2024-10-09 15:10:05 +0200

    connexion version finale (@)

--- connexion.php
+++ connexion.php
@@ -14,22 +14,22 @@ foreach ($connexionData["hosts"] as $hosts) {
     $bdd = $hosts[1];
     $usr = $hosts[2];
     $pwd = $hosts[3];
-
+    
     // Connexion à la base de données
-    $link = mysqli_connect($host, $usr, $pwd, $bdd);
+    $link = @mysqli_connect($host, $usr, $pwd, $bdd); // Le @ permet de retirer l'affichage du message d'erreur (l'instruction ne comprends pas try catch)
 
     // Vérification de la connexion
     if ($link) {
-        echo "Connexion réussie à la base de données $bdd sur $host.";
+        // echo "Connexion réussie à la base de données $bdd sur $host. \n";
         break; // On sort de la boucle si la connexion est réussie
     } else {
         // Affichage d'une erreur pour le debug
-        error_log("Échec de la connexion à $host: " . mysqli_connect_error());
-        echo "Échec de la connexion à $host: " . mysqli_connect_error();
+        // error_log("Échec de la connexion à $host: " . mysqli_connect_error() . "\n");
+        // echo "Échec de la connexion à $host: " . mysqli_connect_error() . "\n";
     }
 }
 
 if (!$link) {
-    die("Connexion échouée : " . mysqli_connect_error());
+    die("Connexion échouée : " . mysqli_connect_error() . "\n");
 }
 ?>
